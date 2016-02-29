be4b788d1e9954753daf25c74d9fcd0a5521a78d
syl <yuelong.su@quantum-info.com>
2016-02-29 11:03:41 +0800

    3

--- testthreadpool_main.cpp
+++ testthreadpool_main.cpp
@@ -15,12 +15,12 @@ int main()
 {
 	ThreadPool pool(3);
 	std::vector<Student> students {{"lucy",18},{"lily",5},{"hanmeimei",8},{"",30},{"lilei",14},{"xiaoming",3},{"xiaohong",12}};
-	std::vector<std::future<int>> results;
+	std::vector<int> results;
 
 	for(auto &student : students)
 	{
 		auto res = pool.enqueue(
-				[&student]()->int {
+				[student]()->int {
 				if(student.name_.empty())
 				{
 				    std::cout<<"input a empty student name!"<<std::endl;
@@ -28,19 +28,21 @@ int main()
 				}
 				if(student.age_ >= 10)
 				{
-				    std::cout<<"larger than 10 age,name:"<<student.name_<<std::endl;
+					std::cout<<"larger than 10 age,name:"<<student.name_<<std::endl;
 				}
 				else
 				{
-				    std::cout<<"smaller than 10 age,name:"<<student.name_<<std::endl;
+					std::cout<<"smaller than 10 age,name:"<<student.name_<<std::endl;
 				}
 				return 0;
 				});
+
+		results.push_back(res.get());
 	}
 
 	for(auto &res : results)
 	{
-		std::cout<<"result:"<<res.get()<<std::endl;
+		std::cout<<"result:"<<res<<std::endl;
 	}
 
 	return 0;
